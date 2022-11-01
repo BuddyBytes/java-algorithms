90d50343c2f6f25ba9b585cc08f91453809f8523
OUA LID <117022089+BuddyBytes@users.noreply.github.com>
2022-11-01 10:59:22 +0100

    Bubble Sort Code Update

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] BunchOfUnsortedValues = {10,1,2,0,15,9,7};
+        Bubble_Sort(BunchOfUnsortedValues);
+        System.out.println(Arrays.toString(BunchOfUnsortedValues));
+    }
+
+
+    public static void Bubble_Sort(int[] BunchOfUnsortedValues){
+        for (int mainElement = 0; mainElement < BunchOfUnsortedValues.length; mainElement++) {
+            // checking every value with the mainElement
+            for (int secondElement = 1; secondElement < BunchOfUnsortedValues.length-mainElement; secondElement++) {
+                // checking and swapping the values
+                if(BunchOfUnsortedValues[secondElement-1] > BunchOfUnsortedValues[secondElement]){
+                    int temp = BunchOfUnsortedValues[secondElement-1];
+                    BunchOfUnsortedValues[secondElement-1] = BunchOfUnsortedValues[secondElement];
+                    BunchOfUnsortedValues[secondElement] = temp;
+                }
+            }
+        }
+    }
+}
