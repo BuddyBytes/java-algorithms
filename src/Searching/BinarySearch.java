ea19cbf9ad50e441c7f95f200be7970fa4589e1d
OUA LID <117022089+BuddyBytes@users.noreply.github.com>
2022-11-02 20:03:08 +0100

    Binary search Code optimization

--- src/Searching/BinarySearch.java
+++ src/Searching/BinarySearch.java
@@ -3,13 +3,20 @@ package Searching;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] BunchOfValues = {0,1,7,8,9,10,11};
-        int targetValue = 17;
+        int[] SecondBunchOfValues = {11,10,9,8,7,1,0};
+        int targetValue = 7;
         boolean result = Binary_Search(BunchOfValues,targetValue);
         System.out.println(result);
+        boolean result_1 = Binary_Search(SecondBunchOfValues,targetValue);
+        System.out.println(result_1);
     }
 
 
     public static boolean Binary_Search(int[] BunchOfSortedValues ,int TargetValue){
+        boolean descendingOrInverse = false;
+        if(BunchOfSortedValues[0] > BunchOfSortedValues[BunchOfSortedValues.length-1]){
+            descendingOrInverse = true;
+        }
         int FirstIndexOfSortedValues = 0;
         int LastIndexOfSortedValues = BunchOfSortedValues.length - 1;
 
@@ -21,11 +28,22 @@ public class BinarySearch {
             if(TargetValue == BunchOfSortedValues[MiddleOfSortedValues]){
                 return true;
             }
-            else if(TargetValue < BunchOfSortedValues[MiddleOfSortedValues]){
-                LastIndexOfSortedValues = MiddleOfSortedValues - 1;
+
+            if(!descendingOrInverse){
+                if(TargetValue < BunchOfSortedValues[MiddleOfSortedValues]){
+                    LastIndexOfSortedValues = MiddleOfSortedValues - 1;
+                }
+                else {
+                    FirstIndexOfSortedValues = MiddleOfSortedValues + 1;
+                }
             }
             else {
-                FirstIndexOfSortedValues = MiddleOfSortedValues + 1;
+                if(TargetValue < BunchOfSortedValues[MiddleOfSortedValues]){
+                    FirstIndexOfSortedValues = MiddleOfSortedValues + 1;
+                }
+                else {
+                    LastIndexOfSortedValues = MiddleOfSortedValues - 1;
+                }
             }
 
         }
