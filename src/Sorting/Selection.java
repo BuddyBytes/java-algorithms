cc440bbf835807d3db2d64d7ed99d4d19fe6cd15
OUA LID <117022089+BuddyBytes@users.noreply.github.com>
2022-11-02 19:51:20 +0100

    Selection Sort Code Update

---
+++ src/Sorting/Selection.java
@@ -0,0 +1,49 @@
+package Sorting;
+
+public class Selection {
+    public static void main(String[] args) {
+        int[] x = {8,7,6,1,0};
+        InsertionSort(x);
+        System.out.println();
+    }
+
+    public static void InsertionSort(int[] BunchOfUnsortedValues){
+        for (int eachIndexOfUnsortedValues = 0; eachIndexOfUnsortedValues < BunchOfUnsortedValues.length; eachIndexOfUnsortedValues++) {
+            int maxElement = maxElementInUnsortedArray(BunchOfUnsortedValues,BunchOfUnsortedValues.length-eachIndexOfUnsortedValues);
+            if(maxElement == Integer.MAX_VALUE){
+                System.out.println("all sorted");
+                return;
+            }
+            swapRightPosition(BunchOfUnsortedValues, maxElement,BunchOfUnsortedValues.length-eachIndexOfUnsortedValues-1);
+
+        }
+
+    }
+
+
+
+    private static int maxElementInUnsortedArray(int[] UnsortedArray,int lastIndexOfUnsortedArray) {
+        int indexOfMaxValue = 0;
+        boolean rightPosition = false;
+        for (int firstIndexOfSecondUnsortedArray = 1 ; firstIndexOfSecondUnsortedArray < lastIndexOfUnsortedArray; firstIndexOfSecondUnsortedArray++) {
+            if(UnsortedArray[indexOfMaxValue] < UnsortedArray[firstIndexOfSecondUnsortedArray]){
+                rightPosition = true;
+                indexOfMaxValue = firstIndexOfSecondUnsortedArray;
+            }
+        }
+        if (!rightPosition){
+            return Integer.MAX_VALUE;
+        }
+        
+        return indexOfMaxValue;
+    }
+
+    private static void swapRightPosition(int[] UnsortedArray,int maxElement, int lastIndex) {
+        int temp = UnsortedArray[maxElement];
+        UnsortedArray[maxElement] = UnsortedArray[lastIndex];
+        UnsortedArray[lastIndex] = temp;
+
+    }
+
+
+}
